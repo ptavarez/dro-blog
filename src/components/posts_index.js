be51d1e0fd00a190a7cb9e8a94be137abebac8a0
ptavarez16 <ptavarez16@yahoo.com>
2018-09-11 20:47:16 -0400

    React Router - Import BrowserRouter and Route from react-router-dom - Create first route - Create new component file, posts_index, and import it to use for first route

---
+++ src/components/posts_index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'React';
+
+class PostsIndex extends Component {
+  render() {
+    return (
+      <div>
+        Posts Index
+      </div>
+    );
+  }
+}
+
+export default PostsIndex;
