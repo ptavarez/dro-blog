c111bad4c4e7e03b9355421f521ba12e2b100525
ptavarez16 <ptavarez16@yahoo.com>
2018-09-13 20:58:19 -0400

    Add form fields - Import needed methods from redux form - Add title, categories, and content fields

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+  
   render() {
     return (
-      <div>
-        PostsNew!
-      </div>
+      <form>
+        <Field
+          label="Title"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Content"
+          name="tags"
+          component={this.renderField}
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+function validate(values) {
+   const errors = {}
+   
+   if(!values.title) {
+     errors.title = "Enter a title";
+   }
+   if(!values.categories) {
+     errors.title = "Enter some categories";
+   }
+   if(!values.content) {
+     errors.title = "Enter some content";
+   }
+   
+   return errors
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(PostsNew);
