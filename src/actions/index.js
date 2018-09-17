480093945644c78ba0359167ffc45787a1ea9d60
ptavarez16 <ptavarez16@yahoo.com>
2018-09-16 22:42:43 -0400

    Link to and delete a post - Add link to a post - Add delete button to delete individual post

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 export const FETCH_POST = 'fetch_post';
-const CREATE_POST = 'create_post';
+export const CREATE_POST = 'create_post';
+export const DELETE_POST = 'delete_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api/posts';
 const API_KEY = '?key=DROgon0519';
@@ -18,7 +19,7 @@ export function fetchPosts() {
 
 export function createPost(values, callback) {
   const request = axios.post(`${ROOT_URL}/${API_KEY}`, values)
-    .then(() => callback())
+    .then(() => callback());
   
   return {
     type: CREATE_POST,
@@ -29,8 +30,20 @@ export function createPost(values, callback) {
 export function fetchPost(id) {
   const request = axios.get(`${ROOT_URL}/${id}${API_KEY}`);
   
+  
   return {
     type: FETCH_POST,
     payload: request
+  };
+}
+
+export function deletePost(id, callback) {
+  const request = axios.delete(`${ROOT_URL}/${id}${API_KEY}`)
+    .then(() => callback());
+    
+    
+  return {
+    type: DELETE_POST,
+    payload: id
   };
 }
